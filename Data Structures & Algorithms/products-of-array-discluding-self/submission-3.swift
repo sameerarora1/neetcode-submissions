b0f87e49badcbe86b38e17b2243b730aab9a7121
Sameer Arora <145398394+sameerarora1@users.noreply.github.com>
2026-04-01 05:29:23 +0200

    Add: products-of-array-discluding-self - submission-3

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-3.swift
@@ -0,0 +1,21 @@
+class Solution {
+    func productExceptSelf(_ nums: [Int]) -> [Int] {
+        var out = Array<Int>(repeating: 0, count: nums.count)
+        var pre = 1
+        var post = 1
+        var i = 0
+        while (i < nums.count) {
+            out[i] = pre
+            pre = pre * nums[i]
+            i += 1
+        }
+        i = nums.count - 1
+        while (i >= 0) {
+            out[i] = post * out[i]
+            post = post * nums[i]
+            i -= 1
+        }
+
+        return out
+    }
+}
